2957777f8439ec1d2961927468b4f608a2380372
Johnathan Yan <johnathanyan@gmail.com>
2014-06-04 08:25:30 -0400

    added movement

--- Entity.java
+++ Entity.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 public class Entity {
     
     protected int _hp;
@@ -11,5 +13,52 @@ public class Entity {
    
     public void setX(int x) { _xcor = x; }
     public void setY(int y) { _ycor = y; } 
+
+    public boolean isJumping;
+    public boolean isFalling;
+
+    public int gravity = 10;
+    public float dx;
+    public float dy;
+
+    public thread thread;
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -1;
+        }
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 1;
+        }
+        if (key == KeyEvent.VK_UP) {
+            dy = -1;
+        }
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 1;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+
+    public void moving(){
+    	_xCor += dx;
+    	_ycor += dy;
+    }
+	    
 
 }
