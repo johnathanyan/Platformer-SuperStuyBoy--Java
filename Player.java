7788d02b6e547a6edf3b58d51edb780c2216bbe2
Hubert <hubertpuszklewicz@gmail.com>
2014-06-08 17:45:58 -0400

    Changed movement to be based on booleans
    Booleans make it easier to see the direction the player is moving in

--- Player.java
+++ Player.java
@@ -4,46 +4,48 @@ import java.io.File;
 
 public class Player {
 
-	private double x;
-	private double y;
-	private double xSpeed;
-	private double ySpeed;
+    private double x;
+    private double y;
+    private double xSpeed;
+    private double ySpeed;
 
-	private int health;
+    private int health;
 
-	private String direction; // left or right
-	private boolean isJumping;
+    private boolean isJumping, left, right;
+    //I changed the direction to a boolean which is easier to control
+    //in the keylistener. When the key is released you can just set it to false
+    //instead of having to use another string for no movement.
 
-	private double walkingSpeed;
-	private double xMax; 
-	private double yMax; // max velocities for velocities
-	private double resistance;
-	private double gravity;
+    private double walkingSpeed;
+    private double xMax; 
+    private double yMax; // max velocities for velocities
+    private double resistance;
+    private double gravity;
 
-	public void move(){ // continously call this w/ a thread
-		if(direction.equals("left")){
-			xSpeed = xSpeed - walkingSpeed;
-			if (xSpeed < (xMax * -1))
-				xSpeed = -1 * xMax;
-		}
-		else if(direction.equals("right")){
-			xSpeed = xSpeed + walkingSpeed;
-			if (xSpeed < (xMax))
-				xSpeed = xMax;
-		}
-		else { // no movement, decceleration
-			if (xSpeed > 0) { // if original direction is right
-				xSpeed -= resistance;
-				if (xSpeed < 0){
-					xSpeed = 0;
-				}
-			}
-			if (xSpeed < 0) { // if original direction is left
-				xSpeed -= resistance;
-				if (xSpeed > 0){
-					xSpeed = 0;
-				}
-			}
-		}
+    public void move(){ // continously call this w/ a thread
+	if(left){
+	    xSpeed = xSpeed - walkingSpeed;
+	    if (xSpeed < (xMax * -1))
+		xSpeed = -1 * xMax;
 	}
+	else if(right){
+	    xSpeed = xSpeed + walkingSpeed;
+	    if (xSpeed < (xMax))
+		xSpeed = xMax;
+	}
+	else { // no movement, decceleration
+	    if (xSpeed > 0) { // if original direction is right
+		xSpeed -= resistance;
+		if (xSpeed < 0){
+		    xSpeed = 0;
+		}
+	    }
+	    if (xSpeed < 0) { // if original direction is left
+		xSpeed -= resistance;
+		if (xSpeed > 0){
+		    xSpeed = 0;
+		}
+	    }
+	}
+    }
 }
