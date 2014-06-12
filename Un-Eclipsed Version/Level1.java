b2051298929c406bee3e312cd432c8f4bf60212d
thlebowicz <thomashlebowicz1@gmail.com>
2014-06-11 20:10:11 -0400

    Minor fix- Tiles printed upon startup

--- Un-Eclipsed Version/Level1.java
+++ Un-Eclipsed Version/Level1.java
@@ -5,8 +5,10 @@ import java.awt.event.KeyEvent;
 public class Level1 extends Level{
     private Background bg;
     private Player player;
+    private TileMap tiles;
     
     public Level1(Player p){
+	tiles = new TileMap("test.txt",33);
 	try{
 	    bg = new Background("/Backgrounds/level1.gif", 1);
 	}catch(Exception e){
@@ -27,12 +29,14 @@ public class Level1 extends Level{
     }
     public void draw(java.awt.Graphics2D g){
 	bg.draw(g);
+	tiles.drawTiles("testsprite.png",g);
     }
 
     private void select() {
     }
 
     public void keyPressed(int k){
+	System.out.println(tiles.getMap());
 	if (k == KeyEvent.VK_SPACE) 
 	    player.jump();
 	else if (k == KeyEvent.VK_RIGHT) 
