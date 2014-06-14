583d7a9754c7295958b4238a47cdcfb436fc0dc3
Hubert <hubertpuszklewicz@gmail.com>
2014-06-14 13:59:56 -0400

    Tile class + sprite rendering changes

---
+++ Un-Eclipsed Version/Tile.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.awt.image.BufferedImage;
+import java.awt.*;
+import javax.imageio.*;
+
+public class Tile{
+    private int left, right;
+    private int bot, top;
+    private int x, y, size;
+    private BufferedImage sprite;
+
+    public Tile(int x, int y, int size, BufferedImage sprite){
+    }
+}
--- Un-Eclipsed Version/TileMap.java
+++ Un-Eclipsed Version/TileMap.java
@@ -7,13 +7,12 @@ import javax.imageio.*;
 public class TileMap{
     private int tileSize, mapWidth, mapHeight;
     private int x, y;
-    private int[][] map, tiles;
+    private int[][] map;
+    private Tile[][] tiles;
     private BufferedImage[] sprites;
     private BufferedImage spriteSheet;
     private Scanner sc;
     private File file;
-
-   
 
     //reads file of ints to determine which sprites to use
     public TileMap(String path, String sheetPath, int tileSize){
@@ -29,10 +28,10 @@ public class TileMap{
 	mapWidth = Integer.parseInt(sc.nextLine());
 	mapHeight = Integer.parseInt(sc.nextLine());
 	map = new int[mapHeight][mapWidth];
+	tiles = new Tile[mapHeight][mapWidth];
 	sprites = new BufferedImage[mapHeight * mapWidth];
 
 	String toSplit = "\\s+";
-
 	for(int i = 0; i < mapHeight; i++){
 	    String line = sc.nextLine();
 	    String[] elements = line.split(toSplit);
@@ -49,33 +48,44 @@ public class TileMap{
 	divideSheet(spriteSheet);
     }
 
+    //splits up sheet into smaller images to be used for tiles/entities
     public void divideSheet(BufferedImage sheet){
 	int count = 0;
 	int w = sheet.getWidth();
 	int h = sheet.getHeight();
-	for(int m = 0; m < (w - tileSize); m += tileSize){
-	    for(int n = 0; n < (h - tileSize); n += tileSize){
+	for(int m = 0; m < (w - tileSize); m += tileSize + 1){
+	    for(int n = 0; n < (h - tileSize); n += tileSize + 1){
 		System.out.println(m + ", " + n);
 		sprites[count] = sheet.getSubimage(m, n, tileSize, tileSize);
 		count++;
 	    }
 	}
     }
-		
-	
-	
 
+    //actually draws the tiles on the screen depending on the int[][]
+    //made in the contructor
     public void drawTiles(Graphics2D g){
 	x = 0;
 	y = 0;
+	int count = 0;
+	int sprite = 0;
 	for(int i = 0; i < mapHeight; i++){
 	    x = 0;
 	    for(int j = 0; j < mapWidth; j++){
-		if(map[i][j] == 1){
-		    g.drawImage(sprites[0], x, y, null);
-		}else{
-		    g.drawImage(sprites[1], x, y, null);
+		int type = map[i][j];
+		if(type == 0){
+		    g.drawImage(sprites[3], x, y, null);
+		    sprite = 3;
 		}
+		else if(type == 1){
+		    g.drawImage(sprites[0], x, y, null);
+		    sprite = 0;
+		}
+		else if(type == 2){
+		    g.drawImage(sprites[6], x, y, null);
+		    sprite = 6; 
+		}
+		tiles[i][j] = new Tile(x, y, tileSize, sprites[sprite]);
 		x += tileSize; //move column
             }
 	    y += tileSize; //moves over row when 1 is finished (now it should start at 0,0)
@@ -87,4 +97,3 @@ public class TileMap{
     }
 
 }
-	
